2980d046a47042ed935ca6e1ced47ce785b374c1
Kushagra-Gupta-2607 <98476315+Kushagra-Gupta-2607@users.noreply.github.com>
2022-02-20 11:18:30 +0530

    Update Number of 1 bits.cpp

--- Bit Manipulation/Number of 1 bits.cpp
+++ Bit Manipulation/Number of 1 bits.cpp
@@ -4,5 +4,25 @@ class Solution {
   public:
     int setBits(int N) {
         return __builtin_popcount(N);
+      
+        /*
+        Brian Kernighan's Algorithm:
+        int count = 0;
+        while(N){
+          N&=(N-1);
+          count++;
+        }
+        return count;
+        */
+      
+        /*
+        Lookup table: 8 bits of chunks
+        int l_table[256];
+        l_table[0] = 0;
+        l_table[i] = i&1 + l_table[i/2];
+        now to find the answer for N, 
+        ans = l_table[N & 0xff] + l_table[(N>>8) & 0xff] + 
+              l_table[(N>>16) & 0xff] + l_table[(N>>24) & 0xff];
+        */
     }
 };
